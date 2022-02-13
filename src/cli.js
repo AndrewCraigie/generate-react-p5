a810499c9a6559164282db538787ee7b5a019c66
AndrewCraigie <andrew@craigie.co>
2022-02-13 12:58:47 +0000

    Removed console.log of wrapper relative path

--- src/cli.js
+++ src/cli.js
@@ -149,8 +149,6 @@ const generateReactp5 = (argv) => {
     const generatorRelativePath = relativeUps + matchedRootPathObj.generatorPathTail.replace('\\', '/');
     const sketchClosureFileRelativePath = `./${sketchClosureFileName}`;
 
-    console.log(`wrapper relative path: ${generatorRelativePath}`);
-
     const sketchComponentContent = sketchComponentTemplate.sketchComponentTemplate(
         generatorRelativePath,
         sketchClosureName,
